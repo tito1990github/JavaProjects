f13b086c275c25e987e9959b50444c74780f943c
tito1990github <titondomee@gmail.com>
2019-08-04 10:22:23 +0300

    Import only java.io.*; and remove all other imports

--- SimpleCalculator2/src/SimpleCalculator.java
+++ SimpleCalculator2/src/SimpleCalculator.java
@@ -3,9 +3,7 @@
  * Uses BufferedReader object to capture user input. 
  */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class SimpleCalculator {
 
